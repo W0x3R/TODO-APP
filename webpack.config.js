843cfa6e793ab3976882b514deb4245bcc23597c
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-06-27 20:42:41 +0300

    refactor: removed unnecessary configuration in the webpack.config.js file (Fri, Jun 27, 2025, 8:42:34 PM)

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,6 @@ module.exports = {
 					{ from: './src/images/fav-icons', to: 'assets/images/fav-icons' },
 					{ from: './src/site.webmanifest', to: 'site.webmanifest' },
 					{ from: './src/browserconfig.xml', to: 'browserconfig.xml' },
-					{ from: './src/images/lightHouse.jpg', to: 'assets/images/readmePhotos' },
-					{ from: './src/images/readmePhoto.jpg', to: 'assets/images/readmePhotos' }
 				],
 			}),
 		],
