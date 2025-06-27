a21800d4ee94c9b3e945e03df4e641f30b18739c
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-06-27 20:32:55 +0300

    fix: fixed inability to delete task and switch theme and error of displaying checkmark when task is completed in safari browser (Fri, Jun 27, 2025, 8:32:48 PM)

--- src/js/task/createTaskMarkup.js
+++ src/js/task/createTaskMarkup.js
@@ -15,7 +15,7 @@ export const createTaskMarkup = () => {
 			<input class = 'form__list-checkbox' type ='checkbox' id='item_${i}' ${el.checked ? 'checked' : ''}>
 			<label class = 'form__list-label ${formLabelClass}'  for ='item_${i}'><span>${el.value}</span></label>
 
-			<svg class ='form__delete-list' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><polygon class ='form__polygon ${polygonClass}' points="404.176,0 256,148.176 107.824,0 0,107.824 148.176,256 0,404.176 107.824,512 256,363.824 404.176,512 512,404.176 363.824,256 512,107.824"/></svg>
+			<button><svg class ='form__delete-list' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><polygon class ='form__polygon ${polygonClass}' points="404.176,0 256,148.176 107.824,0 0,107.824 148.176,256 0,404.176 107.824,512 256,363.824 404.176,512 512,404.176 363.824,256 512,107.824"/></svg></button>
 
 		</li>`;
 		list.innerHTML = listMessage
